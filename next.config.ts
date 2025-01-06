4bc6ad96bd6e65c018451f51cfb44194ae955469
realSergiy <sergiy.yeskov@gmail.com>
2025-01-06 17:41:01 +1100

    feat: config ts

--- next.config.ts
+++ next.config.ts
@@ -1,3 +1,11 @@
+import type { NextConfig } from 'next';
+import withBundleAnalyzer from '@next/bundle-analyzer';
+import type { RemotePattern } from 'next/dist/shared/lib/image-config';
+
+const bundleAnalyzer = withBundleAnalyzer({
+  enabled: process.env.ANALYZE === 'true',
+});
+
 const VERCEL_BLOB_STORE_ID = process.env.BLOB_READ_WRITE_TOKEN?.match(
   /^vercel_blob_rw_([a-z0-9]+)_[a-z0-9]+$/i,
 )?.[1].toLowerCase();
@@ -14,27 +22,28 @@ const HOSTNAME_AWS_S3 =
       `${process.env.NEXT_PUBLIC_AWS_S3_BUCKET}.s3.${process.env.NEXT_PUBLIC_AWS_S3_REGION}.amazonaws.com`
     : undefined;
 
-const createRemotePattern = hostname =>
+const createRemotePattern = (hostname: string | undefined): RemotePattern[] =>
   hostname
-    ? {
-        protocol: 'https',
-        hostname,
-        port: '',
-        pathname: '/**',
-      }
+    ? [
+        {
+          protocol: 'https',
+          hostname,
+          port: '',
+          pathname: '/**',
+        },
+      ]
     : [];
 
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+const nextConfig: NextConfig = {
   images: {
     imageSizes: [200],
-    remotePatterns: []
-      .concat(createRemotePattern(HOSTNAME_VERCEL_BLOB))
-      .concat(createRemotePattern(HOSTNAME_CLOUDFLARE_R2))
-      .concat(createRemotePattern(HOSTNAME_AWS_S3)),
+    remotePatterns: [
+      ...createRemotePattern(HOSTNAME_VERCEL_BLOB),
+      ...createRemotePattern(HOSTNAME_CLOUDFLARE_R2),
+      ...createRemotePattern(HOSTNAME_AWS_S3),
+    ],
     minimumCacheTTL: 31536000,
   },
 };
 
-module.exports =
-  process.env.ANALYZE === 'true' ? require('@next/bundle-analyzer')()(nextConfig) : nextConfig;
+export default bundleAnalyzer(nextConfig);
