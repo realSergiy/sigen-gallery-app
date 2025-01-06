2be415b57c2be5ce5cf90de17bd1d008d079d25e
realSergiy <sergiy.yeskov@gmail.com>
2025-01-06 17:55:36 +1100

    upgrage packages 0.10.0 (#12)
    * chore: config .ts
    * chore: bump pachages

---
+++ next.config.ts
@@ -0,0 +1,49 @@
+import type { NextConfig } from 'next';
+import withBundleAnalyzer from '@next/bundle-analyzer';
+import type { RemotePattern } from 'next/dist/shared/lib/image-config';
+
+const bundleAnalyzer = withBundleAnalyzer({
+  enabled: process.env.ANALYZE === 'true',
+});
+
+const VERCEL_BLOB_STORE_ID = process.env.BLOB_READ_WRITE_TOKEN?.match(
+  /^vercel_blob_rw_([a-z0-9]+)_[a-z0-9]+$/i,
+)?.[1].toLowerCase();
+
+const HOSTNAME_VERCEL_BLOB = VERCEL_BLOB_STORE_ID
+  ? `${VERCEL_BLOB_STORE_ID}.public.blob.vercel-storage.com`
+  : undefined;
+
+const HOSTNAME_CLOUDFLARE_R2 = process.env.NEXT_PUBLIC_CLOUDFLARE_R2_PUBLIC_DOMAIN;
+
+const HOSTNAME_AWS_S3 =
+  process.env.NEXT_PUBLIC_AWS_S3_BUCKET && process.env.NEXT_PUBLIC_AWS_S3_REGION
+    ? // eslint-disable-next-line max-len
+      `${process.env.NEXT_PUBLIC_AWS_S3_BUCKET}.s3.${process.env.NEXT_PUBLIC_AWS_S3_REGION}.amazonaws.com`
+    : undefined;
+
+const createRemotePattern = (hostname: string | undefined): RemotePattern[] =>
+  hostname
+    ? [
+        {
+          protocol: 'https',
+          hostname,
+          port: '',
+          pathname: '/**',
+        },
+      ]
+    : [];
+
+const nextConfig: NextConfig = {
+  images: {
+    imageSizes: [200],
+    remotePatterns: [
+      ...createRemotePattern(HOSTNAME_VERCEL_BLOB),
+      ...createRemotePattern(HOSTNAME_CLOUDFLARE_R2),
+      ...createRemotePattern(HOSTNAME_AWS_S3),
+    ],
+    minimumCacheTTL: 31536000,
+  },
+};
+
+export default bundleAnalyzer(nextConfig);
